d1ff2b71165b2259645c6918a9fad3742ad6f414
Kaolhou <xdroid42@gmail.com>
2021-08-27 14:36:05 -0300

    alterações em módulos, App.js entre outros

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+//bibliotecas
+import './stylesheets/App.css';
+//import gmdb from './gmdb';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link/*,
+  useRouteMatch,
+  useParams*/
+} from "react-router-dom";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Início</Link>
+          </li>
+          <li>
+            <Link to="/games">Jogos</Link>
+          </li>
+          <li>
+            <Link to="/about">Sobre</Link>
+          </li>
+        </ul>
+
+        <Switch>
+          <Route path="/about">
+            oi
+          </Route>
+          <Route path="/games">
+            alo
+          </Route>
+          <Route path="/">
+            sla
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
